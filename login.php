6b75d1efa044aee1a3a903b00aa351891263852f
Ade Ayu <111202416024@mhs.dinus.ac.id>
2026-01-09 10:02:14 +0700

    menghapus link icon bunga

--- login.php
+++ login.php
@@ -66,7 +66,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
     />
-    <link rel="icon" href="img/Flower Dreams.png" />
     <style>
 
     .custom-alert-width {
